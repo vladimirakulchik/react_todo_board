b454ef6e70f659372a765f41944b0f0522c3e526
Vladimir Akulchik <vladimirakulchyk@gmail.com>
2018-04-02 10:53:45 +0300

    Added truncate for card text

--- src/components/Card/CardDetails.jsx
+++ src/components/Card/CardDetails.jsx
@@ -4,23 +4,34 @@ import IconButton from 'material-ui/IconButton';
 import ModeEdit from 'material-ui/svg-icons/editor/mode-edit';
 import './CardDetails.css';
 
-function CardDetails() {
-    return (
-        <Card className="card-details" style={{backgroundColor: "white"}}>
-            <IconButton className="card-edit-btn">
-                <ModeEdit />
-            </IconButton>
+class CardDetails extends React.Component {
 
-            <CardTitle className="card-title" title="Card title" />
+    static truncate(text) {
+        let maxSize = 350;
+        let result = text;
 
-            <CardText className="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-            </CardText>
-        </Card>
-    );
+        if ((text) && (text.length > maxSize)) {
+            result = text.substr(0, maxSize) + "...";
+        }
+
+        return result;
+    }
+
+    render() {
+        return (
+            <Card className="card-details" style={{backgroundColor: "white"}}>
+                <IconButton className="card-edit-btn">
+                    <ModeEdit />
+                </IconButton>
+
+                <CardTitle className="card-title" title="Card title" />
+
+                <CardText className="card-text">
+                    {CardDetails.truncate("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat.")}
+                </CardText>
+            </Card>
+        );
+    }
 }
 
 export default CardDetails;
